70da0153e66bc0c29e79187572e0c9ebe9cba140
Dogukan (Dan) Mere <dan.mere@voilalearning.com>
2022-12-08 18:27:06 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Clean Project</h1>
+    </div>
+  )
+}
+
+export default App

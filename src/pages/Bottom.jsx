5f0bc3821e0dc6e1c0478d466e0cced9ef7225d7
Dogukan (Dan) Mere <meredogukan@gmail.com>
2022-12-09 12:01:33 -0500

    bottom section done

--- src/pages/Bottom.jsx
+++ src/pages/Bottom.jsx
@@ -8,11 +8,11 @@ const Bottom = () => {
       <div className=' flex-[1] '>
         <img src={imgLeft} alt='dark furniture' />
       </div>
-      <div className='flex-[1] px-[2.5rem] py-[3.5rem] md:px-[2.5rem] md:py-[.2rem]'>
+      <div className='flex-[1] px-[2.5rem] py-[3.5rem] md:px-[1rem] lg:px-[2.5rem] md:py-[.2rem]'>
         <h2 className='uppercase text-black text-start text-[.8rem] lg:text-[] tracking-[.3rem] font-semibold'>
           About our furniture
         </h2>
-        <p className='text-darkGray text-start mt-[1rem] md:mt-[.5rem] lg:text-[.9rem] text-[0.8rem] md:text-[.6rem] font-medium max-w-[24rem]'>
+        <p className='text-darkGray text-start mt-[1rem] md:mt-0 lg:mt-[.5rem] lg:text-[.9rem] text-[0.9rem] md:text-[.7rem] font-medium max-w-[24rem]'>
           Our multifunctional collection blends design and function to suit your
           individual taste. Make each room unique, or pick a cohesive theme that
           best express your interests and what inspires you. Find the furniture
